3b0504d49b413c0f260f6b877811749c48dd02c6
ShabeenaShaikh98 <shabeenashaikh112000@gmail.com>
2026-03-31 01:32:10 +0530

    fix app path final

--- pharma_django/pharmacy_project/settings.py
+++ pharma_django/pharmacy_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "pharmacy_app",
+    "pharma_django.pharmacy_app",
     "ml_engine",
 ]
 
